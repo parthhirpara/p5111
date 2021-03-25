cb01a92209c095078e8312d5bf00c0d18e913549
parth.hirpara <parth.hirpara@iblinfotech.com>
2021-03-25 14:23:30 +0530

    chage

--- TestApp/TestApp/Utility/WsPrefix.swift
+++ TestApp/TestApp/Utility/WsPrefix.swift
@@ -39,11 +39,10 @@ let kStatusNoRecord = "6"
 let kStatusEmailNotAdded = "4"
 
 
-let BASE_URL = //"http://192.168.22.13:8079"
-//"http://3.17.152.93:8082"
-"http://3.137.131.252:8082"
+let BASE_URL =
+""
 
-//let WS_URL = "ws://project.greatwisher.com:9006" // test
+//let WS_URL = "" // test
 
 let BASE_URL_FOR_IMAGE = "\(BASE_URL)/Files/" //test
 
@@ -77,87 +76,4 @@ let kMap_URL                        =   "https://maps.googleapis.com/maps/api/ge
 //MARK:- API URL
 
 let kSignUp             =   "/api/v1/User"
-let kSignupVerify       =   "/api/v1/Account/VerifyEmail"
-let kVerifyResend       =   "/api/v1/User/ResendCode"
-let kVerifyResendForgot =   "/api/v1/User/ResendForgetPasswordCode"
-let kForgotPassword     =   "/api/v1/User/ForgetPassword"
-let kForgotCodeVerify   =   "/api/v1/User/VerifyForgetCode"
-let kSetNewPassword     =   "/api/v1/User/ChangeAccountPassword"
-let kLogin              =   "/api/v1/Account/login"
-let kSetUIDAPI          =   "/api/v1/User/AddUid"
-let kUpdateLocationAPI  =   "/api/v1/User/UpdateLocation"
-let kCheckUserName      =   "/api/v1/User/UsernameExist"
-let kCheckEmailExist    =   "/api/v1/User/EmailExist"
-let kUpdateUserInfo     =   "/api/v1/User/UpdateUserProfile/"
-let kChangePasswordAPI  =   "/api/v1/User/ChangePasswordWithFirebase"
-let kUploadUserProfile  =   "/api/v1/User/UploadProfilePic"
-let kUpdateBioAPI       =   "/api/v1/User/UpdateBio/"
-let kGetMyCircleListAPI =   "/api/v1/User/GetMyConnectionByType"
-let kGetFriendCircleListAPI =   "/api/v1/User/GetFriendsConnectionByType"
-let kSetSettingsDataAPI =   "/api/v1/User/UserSetting"
-let kSetSettingInfoAPI  =   "/api/v1/User/GetUserSetting"
 
-//Privacy
-let kSetImagePostPrivacyAPI =   "/api/v1/Privacy/UpdatePicmeePrivacy"
-let kSetBlogPostPrivacyAPI  =   "/api/v1/Privacy/UpdateBlogPrivacy"
-let kSetVideoPostPrivacyAPI =   "/api/v1/Privacy/UpdateMoviePrivacy"
-let kGetPrivacyListAPI      =   "/api/v1/Privacy/GetPrivacyByUserId/"
-let kUpdatePrivacyListAPI   =   "/api/v1/Privacy/UpdateUserPrivacy"
-
-//Report
-let kPostReportinAPI        =   "/api/v1/User/Report"
-
-//Badges
-let kGetBadgesAPI           =   "/api/v1/User/GetBadge/"
-let kUserClearBadgesAPI     =   "/api/v1/User/ClearBadge/"
-
-//Friend
-let kSendConnectionRequest      =   "/api/v1/Friends/SendConnectionRequest"
-let kGetRequstListAPI           =   "/api/v1/Friends/GetConnectionRequests"
-let kCancelFriendRequestAPI     =   "/api/v1/Friends/CancelConnectionRequest"
-let kCancelFriendPendingRequestAPI  =   "/api/v1/Friends/CancelSendRequest"
-let kAcceptFriendRequestAPI     =   "/api/v1/Friends/RespondToConnectionRequest"
-let kRemoveFriendInCircleAPI    =   "/api/v1/Friends/RemoveConnection"
-let kChangeCircleAPI            =   "/api/v1/Friends/ChangeCircle"
-
-//User
-let kGetUserListAPI     =   "/api/v1/user/FindFriendUser/"
-let kGetUserSearchAPI   =   "/api/v1/User/SearchUser"
-let kGetUserInfoAPI     =   "/api/v1/User"
-let kGetUserInfoFromUIDAPI      =   "/api/v1/User/GetInformationByUid"
-let kGetOtherUserInfoAPI        =   "/api/v1/user/GetFriendUserInformation"
-let kGetUserNotificationAPI     =   "/api/v1/user/GetNotifications"
-let kDeleteNotificationAPI      =   "/api/v1/user/DeleteUserNotification"
-let kUserLogoutAPI              =   "/api/v1/Account/Logout"
-    
-//blog
-let kBlogAddAPI             =   "/api/v1/Blog"
-let kGetBlogListAPI         =   "/api/v1/Blog/GetPaged/"
-let kGetMyBlogListAPI       =   "/api/v1/Blog/GetMyBlogList/"
-let kBlogLikeAPI            =   "/api/v1/Blog/BlogLikes"
-let kGetBlogmeeByIDAPI      =   "/api/v1/blog/GetBlogDetailsById"
-let kDeleteBlogMeeAPI       =   "/api/v1/Blog"
-let kUpdateBlogPostAPI      =   "/api/Blog/UpdateBlog"
-
-//pickmee
-let kGetImagesListAPI   =   "/api/v1/Picmee/"
-let kGetImageByIDAPI    =   "/api/v1/Picmee/GetPicmeeDetailsById"
-let kImagesPostLikeAPI  =   "/api/v1/Picmee/PicmeeLikes"
-let kImagePostUploadAPI =   "/api/v1/Picmee"
-let kMyImagePostListAPI =   "/api/v1/Picmee/GetMyPicmeeList/"
-let kDeletePicMeeAPI    =   "/api/v1/Picmee"
-let kUpdatePicmeePOSTAPI    =   "/api/v1/Picmee/UpdatePicmee"
-
-//videos
-let kVideoPostLikeAPI   =   "/api/v1/Watchmee/WatchmeeLikes"
-let kGetVideosListAPI   =   "/api/v1/Watchmee/GetPaged/"
-let kGetVideosByIDAPI   =   "/api/v1/Watchmee/GetWatchmeeDetailsById"
-let kGetMyVideosListAPI   =   "/api/v1/Watchmee/GetMyWatchmeeList/"
-let kVideoPostUploadAPI =   "/api/v1/Watchmee/AddMovie"
-let kUploadVideoThumb   =   "/api/v1/Watchmee/VideoUpload"
-let kDeleteWhatchmeeAPI =   "/api/v1/Watchmee"
-let kUpdateVideoPostAPI =   "/api/v1/Watchmee/UpdateMovie"
-
-//Story
-let kAddStoryAPI        =   "/api/v1/Story/AddStory"
-let kGetStorysAPI       =   "/api/v1/Story/GetAllStoryList/"
